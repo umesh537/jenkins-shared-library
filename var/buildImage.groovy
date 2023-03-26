e5eaed1f552425085c13bd30f67a020f0b2df364
Umesh Suryavanshi <39472741+umesh537@users.noreply.github.com>
2023-03-26 22:10:50 +0530

    Update buildImage.groovy

--- var/buildImage.groovy
+++ var/buildImage.groovy
@@ -1,11 +1,6 @@
 #!/usr/bin/env groovy
 
 
-def call(){
-    echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]){
-        sh 'docker build -t umeshsurya01/demo-app:jma-3.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push umeshsurya01/demo-app:jma-3.0'
-    }
+def call(String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
